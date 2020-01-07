4de92ca05562e3747c234933eaf484a39c1edfd6
Neah Nieto <nrpcnieto@gmail.com>
2020-01-06 19:02:02 -0600

    CRUD API actions for user_skills controller

--- app/controllers/api/user_skills_controller.rb
+++ app/controllers/api/user_skills_controller.rb
@@ -1,2 +1,32 @@
 class Api::UserSkillsController < ApplicationController
+  def index
+    @user_skills = UserSkill.all
+    render "index.json.jb"
+  end
+
+  def create
+    @user_skill = UserSkill.new(
+      user_id: params[:user_id],
+      skill_id: params[:skill_id],
+    )
+    render "show.json.jb"
+  end
+
+  def show
+    @user_skill = UserSkill.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @user_skill = UserSkill.find_by(id: params[:id])
+    @user_skill.user_id = params[:user_id] || @user_skill.user_id
+    @user_skill.skill_id = params[:skill_id] || @user_skill.skill_id
+    render "show.json.jb"
+  end
+
+  def destroy
+    @user_skill = UserSkill.find_by(id: params[:id])
+    @user_skill.destroy
+    render json: { message: "User skill deleted" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
     get "/skills/:id" => "skills#show"
     patch "/skills/:id" => "skills#update"
     delete "/skills/:id" => "skills#destroy"
+
+    get "/user_skills" => "user_skills#index"
+    post "/user_skills" => "user_skills#create"
+    get "/user_skills/:id" => "user_skills#show"
+    patch "/user_skills/:id" => "user_skills#update"
+    delete "/user_skills/:id" => "user_skills#destroy"
   end
 end
